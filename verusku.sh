7cef04e26f99cc23f1d4cc15d9044346f92f3940
suid28 <104066532+suid28@users.noreply.github.com>
2022-04-21 18:45:48 +0700

    Create verusku.sh

---
+++ verusku.sh
@@ -0,0 +1,5 @@
+# veruscoin
+sudo apt update
+wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
+tar -xvf nheqminer-Linux-v0.8.2.tar.gz
+nheqminer/nheqminer -v -l ap.luckpool.net:3956 -u RT2AQGdcpVXUXzFvbYAqK75J5LzCSWAMBY.vps4 p x -t 4
